c07b3a9ec95509d1ce0b51002fd98398efc5fea6
Charles_G <993634344@qq.com>
2023-03-31 18:06:08 +0800

    Delete packages/dz-design-vue/lib directory

--- packages/dz-design-vue/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-require("./card/index.js");
-require("./switch/index.js");
-const card = require("./card/card.js");
-const _switch = require("./switch/switch.js");
-exports.Card = card;
-exports.Switch = _switch;
-//# sourceMappingURL=index.js.map
