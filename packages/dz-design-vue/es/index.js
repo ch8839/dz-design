cf661314457a8c63cbc03ec88a8b8aadb6cf6744
Charles_G <993634344@qq.com>
2023-03-31 18:05:59 +0800

    Delete packages/dz-design-vue/es directory

--- packages/dz-design-vue/es/index.js
+++
@@ -1,9 +0,0 @@
-import "./card/index.js";
-import "./switch/index.js";
-import { default as default2 } from "./card/card.js";
-import { default as default3 } from "./switch/switch.js";
-export {
-  default2 as Card,
-  default3 as Switch
-};
-//# sourceMappingURL=index.js.map
